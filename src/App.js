bd172099aa4f836fa0250bf219be244947c2db6e
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-06-27 19:26:07 -0400

    configuracion proyecto con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+//importamos componentes
+import Logo from './components/Logo.js';
+import  ModalLogin from './components/ModalLogin';
+
+
+
+
 
 function App() {
   return (
+
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Switch>
+        <Route path='/'exact>
+
+        <Logo/>
+        <button><Link to="/login">Mesero</Link></button>
+        <button><Link to="/login">Chef</Link></button>
+        </Route>
+
+        <Route path='/login'>
+          <ModalLogin/>
+
+</Route>
+
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/ModalLogin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*import {
+  Button,
+  Modal,
+} from 'reactstrap';*/
+
+
+function ModalLogin() {
+
+  return (
+<p>hola</p>
+  );
+}
+
+export default ModalLogin
