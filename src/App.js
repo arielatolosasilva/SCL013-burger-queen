85d3138192528fdcb770cee5ef74ba4df752a985
LoretoGutierrezH <loretocgh@gmail.com>
2020-06-27 20:52:13 -0400

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* import Menu from './components/Menu/Menu';
+import ChefLayout from './components/ChefLayout/ChefLayout';
+import WaitressLayout from './components/WaitressLayout/WaitressLayout'; */
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import 'bootstrap / dist / css / bootstrap.min.css';
+import ModalLogin from './components/ModalLogin/ModalLogin';
+import {
+  Button,
+  ModalHeader,
+  ModalBody,
+  Modal,
+  ModalFooter,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input
+} from 'reactstrap';
+const App = (props) => {
+  /* state = {
+    currency: '$',
+    products: [
+      {id: 'bk1', name: 'Café americano', price: 1000},
+      {id: 'bk2', name: 'Café con leche', price: 2000},
+      {id: 'bk3', name: 'Té matcha', price: 3000}
+    ]
+  } */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    const {
+      buttonLabel,
+      className
+    } = props;
+
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+    
+
+    return (
+      <BrowserRouter>
+        <div>
+        <Button color="danger" onClick={toggle}>Entrar</Button>
+        <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+         <InputGroup>
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText>@</InputGroupText>
+          </InputGroupAddon>
+          <Input placeholder="username" />
+         </InputGroup>
+
+         <InputGroup>
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText>#</InputGroupText>
+          </InputGroupAddon>
+          <Input placeholder="contraseña" />
+         </InputGroup>
+
+         <InputGroup>
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText>Rol</InputGroupText>
+          </InputGroupAddon>
+          <Input placeholder="Mesero/Chef" />
+         </InputGroup>
+         
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Entrar</Button>{' '}
+        </ModalFooter>
+      </Modal>
+        <Switch>
+          <Route path="/login">
+            <ModalLogin />
+            {/* <WaitressLayout />
+            <Menu products={this.state.products} currency={this.state.currency}/> */}
+          </Route>
+          {/* <Route path="/login">
+            <ChefLayout />
+          </Route> */}
+        </Switch>
+        </div>
+      </BrowserRouter>
+    );
 }
 
 export default App;
