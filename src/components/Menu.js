09fd7b41cba6d4a40a2bd366272667d89b7c3f77
LoretoGutierrezH <loretocgh@gmail.com>
2020-07-02 09:40:23 -0400

    cambios para pull request

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,28 +6,45 @@ import Order from './Order/Order';
 import { Route, Redirect } from 'react-router-dom';
 class Menu extends Component {
     state = {
-
+      currency: '$',
+      products: [
+        {id: 'cl', name: 'Café con leche', price: '2000'},
+        {id: 'gz', name: 'Gyozas', price: '3500'}
+        ],
+      total: '5500'
     }
+
     render(){
         let menu = null;
         let breakfastMenu = Object.entries(DataMenu[0])[0][1].products;
-        console.log(typeof(breakfastMenu));
+        console.log(typeof(breakfastMenu)); //array of objects
         menu = breakfastMenu.map(product => {
             return (
-                <React.Fragment key={product.id}>
+              <React.Fragment key={product.id}>
                 <li>
-                    {product.nombre}
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{product.valor}</span>
-            </li>
-            </React.Fragment>
+                  <Order name={product.nombre} price={product.valor}/>
+                   {/*  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{product.valor}</span> */}
+                </li>
+              </React.Fragment>
             )
         })
+       /*  let order = null;
+        order = this.state.products.map((product) => {
+          return (
+          <React.Fragment key={product.id}>
+            <Order name={product.name} price={product.price} />
+          </React.Fragment>
+          )
+        }) */
         return (
             <div>
                 <ul>   
                 {menu}
                 </ul>
-         <Order name="Charlie" product="Gyozas" total="Caleta de plata"/>
+         {/* <Order name="Charlie" product="Gyozas" total="Caleta de plata"/> */}
+
+         {/* {order}
+         <h1>Total: ${this.state.total}</h1> */}
        
             </div>
         );
