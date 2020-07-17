93eef536a0ee5415e4249b6d441072795c64cadd
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-07-16 20:59:29 -0400

    agregando modal vista chef

--- src/components/CardsOrders/CardsIncomingOrders.jsx
+++ src/components/CardsOrders/CardsIncomingOrders.jsx
@@ -1,67 +1,57 @@
-import React from 'react';
-import {
-  Card, CardBody,
-  CardTitle,
-  Button
-} from 'reactstrap';
-import style from '../../components/CardsOrders/CardsIncomingOrder.module.css'
-import olasCard from '../../assets/images/olitascard.png'
-import Contador from '../Contador/Contador';
+import React from "react";
+import { Card, CardBody, CardTitle, Button } from "reactstrap";
+import style from "../../components/CardsOrders/CardsIncomingOrder.module.css";
+import olasCard from "../../assets/images/olitascard.png";
+import Contador from "../Contador/Contador";
 
- const CardsIncomingOrders = (props) => {
-    let orders = null;
-    if (props.data.orders[0].products !== undefined) {
-      orders = props.data.orders.map(order => {
-        console.log(order);
-        let products = order.products;
-        let names = products.map(product => {
-          return (
-          <React.Fragment
-              key={Math.floor(Math.random() * 1000)}><li>{product.name}</li></React.Fragment>
-          )
-        })
-
+const CardsIncomingOrders = (props) => {
+  let orders = null;
+  if (props.data.orders[0].products !== undefined) {
+    orders = props.data.orders.map((order) => {
+      console.log(order);
+      let products = order.products;
+      let names = products.map((product) => {
         return (
           <React.Fragment key={Math.floor(Math.random() * 1000)}>
-
-            <Card style={{ width: '15rem'}, { marginBottom: '4rem' }}   className={style.motherCards}>
-              <CardBody className={style.cardBody}>
-                <CardTitle><p className={style.nTable}>Mesa nro. {order.table} {order.date[0]}</p></CardTitle>
-                  <u>{order.state}</u>
-                  <ul>
-                    {names}
-                  </ul>
-
-              </CardBody>
-
-
-
-
-
-              <div>
-                    <Contador/>
-                    <img src={olasCard} className={style.olasCards} alt='olas'/>
-                    </div>
-             </Card>
-
+            <li>{product.name}</li>
           </React.Fragment>
-        )
-      })
-    }
+        );
+      });
 
-    return (
-      <React.Fragment>
-        <h1 className={style.chef}>Chef</h1>
-        <h2 className={style.incomingOrders}> Pedidos entrantes</h2>
+      return (
+        <React.Fragment key={Math.floor(Math.random() * 1000)}>
+          <Card
+            style={({ width: "15rem" }, { marginBottom: "4rem" })}
+            className={style.motherCards}
+          >
+            <CardBody className={style.cardBody}>
+              <CardTitle>
+                <p className={style.nTable}>
+                  Mesa nro. {order.table} {order.date[0]}
+                </p>
+              </CardTitle>
+              <u>{order.state}</u>
+              <ul>{names}</ul>
+            </CardBody>
 
-      <div className={style.fatherCards}>
+            <div>
+              <Contador />
+              <img src={olasCard} className={style.olasCards} alt="olas" />
+            </div>
+          </Card>
+        </React.Fragment>
+      );
+    });
+  }
 
-          {orders}
+  return (
+    <React.Fragment>
+      <h1 className={style.chef}>Chef</h1>
+      <h2 className={style.incomingOrders}> Pedidos entrantes</h2>
 
-      </div>
-      </React.Fragment>
-    );
-
-}
+      <div className={style.fatherCards}>{orders}</div>
+    </React.Fragment>
+  );
+};
 
 export default CardsIncomingOrders;
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,75 +4,64 @@ import DataMenu from "../../DataMenu.json";
 import { Table } from "reactstrap";
 import style from "./Menu.module.css";
 import OrderModal from "../Order/OrderModal";
-import tea from '../../assets/images/tea.png';
-import rice from '../../assets/images/rice.png';
-
+import tea from "../../assets/images/tea.png";
+import rice from "../../assets/images/rice.png";
 
 class Menu extends Component {
-  state = {
+  state = {};
 
+  arrayProducts = [];
+
+  sumTotal = (array) => {
+    let suma = 0;
+    array.forEach((element) => {
+      let multiplication = element.quantity * element.price;
+      suma += multiplication;
+    });
+    return suma;
   };
 
+  resume = (e) => {
+    let quantityProducts = e.target.value;
+    let quantityFather = e.target.parentElement.parentElement;
+    let quantityFatherChildren = quantityFather.childNodes;
+    let productsName = quantityFatherChildren[0].innerText;
+    let productPrice = quantityFatherChildren[1].outerText;
 
-  arrayProducts = [
+    let productsResume = {
+      name: productsName,
+      price: productPrice,
+      quantity: quantityProducts,
+    };
 
-  ]
+    if (quantityProducts > 0) {
+      this.arrayProducts.push(productsResume);
+      /*console.log(this.arrayProducts)*/
+    } else {
+      let productIndex = this.arrayProducts.findIndex(
+        (product) => product.name === productsResume.name
+      );
+      this.arrayProducts.splice(productIndex, 1);
+      /*console.log(this.arrayProducts)*/
+    }
 
-sumTotal = (array) => {
-  let suma = 0
-  array.forEach(element => {
-    let multiplication = element.quantity * element.price
-    suma += multiplication
+    this.setState({
+      orderId: Math.floor(Math.random() * 8000),
+      products: this.arrayProducts,
+      total: this.sumTotal(this.arrayProducts),
+    });
+  };
 
-  });
-  return suma
-}
+  table = (e) => {
+    let tableNumber = e.target.value;
+    this.setState({
+      table: tableNumber,
+    });
+  };
 
-resume = (e) => {
-  let quantityProducts = e.target.value
-  let quantityFather = e.target.parentElement.parentElement
-  let quantityFatherChildren = quantityFather.childNodes
-  let productsName = quantityFatherChildren[0].innerText
-  let productPrice = quantityFatherChildren[1].outerText
-
-
- let productsResume = {
-   name: productsName,
-   price: productPrice,
-   quantity: quantityProducts,
- }
-
- if(quantityProducts > 0){
-  this.arrayProducts.push(productsResume)
-  /*console.log(this.arrayProducts)*/
- } else {
-  let productIndex = this.arrayProducts.findIndex(product => product.name === productsResume.name)
-  this.arrayProducts.splice(productIndex, 1)
-  /*console.log(this.arrayProducts)*/
- }
-
- this.setState(
-   {
-    orderId: Math.floor(Math.random() * 8000),
-    products: this.arrayProducts,
-    total: this.sumTotal(this.arrayProducts),
-
-
-   }
-   )
-}
-
-table = (e) => {
-  let tableNumber = e.target.value
-  this.setState({
-    table: tableNumber
-  })
-}
-
-
-componentDidUpdate(){
-  console.log(this.state)
-}
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
   render() {
     // Uno u otro menú se guarda aquí dependiendo del resultado del if/else if
@@ -86,15 +75,18 @@ componentDidUpdate(){
     let drinksTitle = null;
     let extrasTitle = null;
 
-
-
     // Condicional según la propiedad type pasada en App.js al comp. Menu
     if (this.props.type === "breakfast") {
       let menuArray = Object.entries(DataMenu[0])[0][1].products;
       currentMenu = menuArray.map((product) => {
         return (
           <React.Fragment key={product.id}>
-            <Product id={product.id} name={product.nombre} price={product.valor} change={(e)=>this.resume(e)}/>
+            <Product
+              id={product.id}
+              name={product.nombre}
+              price={product.valor}
+              change={(e) => this.resume(e)}
+            />
           </React.Fragment>
         );
       });
@@ -108,93 +100,120 @@ componentDidUpdate(){
       let menuArrayExtras = Object.entries(DataMenu[0])[1][1].extras;
       /*console.log(menuArrayExtras)*/
 
-
       //Esto es solo de prueba, hay que cambiarlo por el mapeo del menú almuerzo/cena
       mainCourse = menuArrayMainCourse.map((product) => {
         if (menuArrayMainCourse.length > 0) {
-          mainCourseTitle = 'PLATO DE FONDO'
+          mainCourseTitle = "PLATO DE FONDO";
         }
         return (
           <React.Fragment key={product.id}>
-            <Product id={product.id} name={product.nombre} price={product.valor} change={(e)=>this.resume(e)}/>
+            <Product
+              id={product.id}
+              name={product.nombre}
+              price={product.valor}
+              change={(e) => this.resume(e)}
+            />
           </React.Fragment>
         );
-      })
+      });
 
-    sideDish =menuArraySideDish.map((product) => {
-      if (menuArraySideDish.length > 0) {
-        sideDishTitle = 'ACOMPAÑAMIENTOS'
-      }
-      return (
-        <React.Fragment key={product.id}>
-          <Product id={product.id} name={product.nombre} price={product.valor} change={(e)=>this.resume(e)}/>
-        </React.Fragment>
-      );
-    })
-    drinks = menuArrayDrinks.map((product) => {
-      if (menuArrayDrinks.length > 0) {
-        drinksTitle = 'BEBESTIBLES'
-      }
-      return (
-        <React.Fragment key={product.id}>
-          <Product id={product.id} name={product.nombre} price={product.valor} change={(e)=>this.resume(e)}/>
-        </React.Fragment>
-      );
-    })
-    extras=menuArrayExtras.map((product) => {
-      if (menuArrayExtras.length > 0) {
-        extrasTitle = 'EXTRAS'
-      }
-      return (
-        <React.Fragment key={product.id}>
-          <Product id={product.id} name={product.nombre} price={product.valor} change={(e)=>this.resume(e)}/>
-        </React.Fragment>
-      );
-    })
-   }
-   // RETURN PRINCIPAL DEL COMPONENTE MENÚ
+      sideDish = menuArraySideDish.map((product) => {
+        if (menuArraySideDish.length > 0) {
+          sideDishTitle = "ACOMPAÑAMIENTOS";
+        }
+        return (
+          <React.Fragment key={product.id}>
+            <Product
+              id={product.id}
+              name={product.nombre}
+              price={product.valor}
+              change={(e) => this.resume(e)}
+            />
+          </React.Fragment>
+        );
+      });
+      drinks = menuArrayDrinks.map((product) => {
+        if (menuArrayDrinks.length > 0) {
+          drinksTitle = "BEBESTIBLES";
+        }
+        return (
+          <React.Fragment key={product.id}>
+            <Product
+              id={product.id}
+              name={product.nombre}
+              price={product.valor}
+              change={(e) => this.resume(e)}
+            />
+          </React.Fragment>
+        );
+      });
+      extras = menuArrayExtras.map((product) => {
+        if (menuArrayExtras.length > 0) {
+          extrasTitle = "EXTRAS";
+        }
+        return (
+          <React.Fragment key={product.id}>
+            <Product
+              id={product.id}
+              name={product.nombre}
+              price={product.valor}
+              change={(e) => this.resume(e)}
+            />
+          </React.Fragment>
+        );
+      });
+    }
+    // RETURN PRINCIPAL DEL COMPONENTE MENÚ
     return (
       <React.Fragment>
         <section className={style.menu}>
           <div className={style.text}>
+            <div className={style.contenedorTabla}>
+              <span className={style.table}> N° Mesa </span>
+              <select onChange={(e) => this.table(e)} min={0} max={10}>
+                <option value="0">0 </option>
+                <option value="1">1 </option>
+                <option value="2">2 </option>
+                <option value="3">3 </option>
+                <option value="4">4 </option>
+                <option value="5">5 </option>
+                <option value="6">6 </option>
+                <option value="7">7 </option>
+                <option value="8">8 </option>
+                <option value="9">9 </option>
+                <option value="10">10 </option>
+              </select>
+              <Table borderless className={style.menu}>
+                <tbody className={style.container}>
+                  {/* se vuelve a evaluar para mostrar solo 1 menú por vista*/}
+                  {this.props.type === "breakfast" ? currentMenu : null}
 
-          <div className={style.contenedorTabla}>
-          <span className={style.table}> N° Mesa </span>
-          <select onChange={(e)=>this.table(e)} min={0} max={10} >
-             <option value='0'>0 </option>
-             <option value='1'>1 </option>
-             <option value='2'>2 </option>
-             <option value='3'>3 </option>
-             <option value='4'>4 </option>
-             <option value='5'>5 </option>
-             <option value='6'>6 </option>
-             <option value='7'>7 </option>
-             <option value='8'>8 </option>
-             <option value='9'>9 </option>
-             <option value='10'>10 </option>
-             </select>
-            <Table borderless className={style.menu} >
-
-              <tbody className={style.container}>
-                {/* se vuelve a evaluar para mostrar solo 1 menú por vista*/}
-                {this.props.type === 'breakfast' ? currentMenu : null}
-
-                  <tr><td>{mainCourseTitle}</td></tr>
-                    {mainCourse}
-                  <tr><td>{sideDishTitle}</td></tr>
-                    {sideDish}
-                  <tr><td>{drinksTitle}</td></tr>
-                    {drinks}
-                  <tr><td>{extrasTitle}</td></tr>
-                    {extras}
-
-              </tbody>
-
-            </Table>
-          </div>
+                  <tr>
+                    <td>{mainCourseTitle}</td>
+                  </tr>
+                  {mainCourse}
+                  <tr>
+                    <td>{sideDishTitle}</td>
+                  </tr>
+                  {sideDish}
+                  <tr>
+                    <td>{drinksTitle}</td>
+                  </tr>
+                  {drinks}
+                  <tr>
+                    <td>{extrasTitle}</td>
+                  </tr>
+                  {extras}
+                </tbody>
+              </Table>
+            </div>
           </div>
           <img src={tea} className={style.tea} alt="taza de té matcha"></img>
-          <img src={rice} className={style.rice} alt="onigiri (bolas de arroz)"></img>
+          <img
+            src={rice}
+            className={style.rice}
+            alt="onigiri (bolas de arroz)"
+          ></img>
         </section>
 
         <OrderModal order={this.state} />
